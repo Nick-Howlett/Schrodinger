5d36bf9f6c3f4e5552fa7af643effd27b933822b
Nick Howlett <agiel882@gmail.com>
2019-02-02 23:51:46 -0800

    Added role addition/deletion

--- schrodinger.js
+++ schrodinger.js
@@ -1,39 +1,47 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+
+var client = new Discord.Client();
+
+client.login(auth.token);
+
+client.on("ready", () => {
+    console.log("I am ready!");
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const prefix = "!";
+   
+client.on("message", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    var text = message.content.slice(1);
+    var args = text.split(" ");
+    switch(args[0]) {
+
+        case "ping":
+            message.channel.send("Pong!");
+        break;
+
+        case "bandanna":
+            message.channel.send("Q: How should I wear my bandana? \n A: If you\'re a human, the bandana must be around your upper arm over all layers of clothing.  If you\'re a zombie, the bandana can be worn either around your neck or your head. Bandanas which are not visible from 360°  are not legally worn and tags by zombies without legal bandana placement will be discounted The bandana must be worn at all times except when leaving campus.");
+        break;
+
+        case "addrole":
+            var user = client.users.find(user => user.username === args[1]);
+            var guild_user = message.guild.members.get(user.id);
+            var role = message.guild.roles.find(role => role.name === args[2]);
+            guild_user.addRole(role.id);
+        break; 
+
+        case "deleterole":
+        var user = client.users.find(user => user.username === args[1]);
+        var guild_user = message.guild.members.get(user.id);
+        var role = guild_user.roles.find(role => role.name === args[2]);
+        guild_user.removeRole(role.id);
+        break;           
+    };
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+
+function find_by_name(client, username){
+    
+}
