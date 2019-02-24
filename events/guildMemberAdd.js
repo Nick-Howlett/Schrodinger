e158ed9104de6fe4f6f5570defc14b9931444561
unknown <agiel882@gmail.com>
2019-02-24 15:05:02 -0800

    Add tag reminder on unrecognized tag login

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,12 +1,12 @@
 module.exports = (client, member) => {
-    const welcomeNoTag = `Welcome to the server! We don't recognize your discord tag! If you're interested in joining the human or zombie channels, please go to the current game's page at www.uchicagohvz.org and enter your discord tag!`;
+    const welcomeNoTag = ;
     client.playerdb.find_user(member.user.tag, row =>{
         if(row){
             row.human ? member.addRole(client.guildconstants[member.guild.id].human) : member.addRole(client.guildconstants[member.guild.id].zombie); 
             member.send(`Welcome to the server! You have been assigned a role of ${row.human ? "human" : "zombie"} based on your discord tag!`);
         }
         else{
-            member.send(welcomeNoTag);
+            member.send(`Welcome to the server! We don't recognize your discord tag! If you're interested in joining the human or zombie channels, please go to the current game's page at www.uchicagohvz.org and enter your discord tag! For reference, your discord tag is: ${member.user.tag}`);
         }
     });
 }
