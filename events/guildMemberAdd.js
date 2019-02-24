50509cd357e6b8a65b9f8eab457612f568497927
unknown <agiel882@gmail.com>
2019-02-24 15:00:14 -0800

    Fix event handler, players registered correctly

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,4 @@
 module.exports = (client, member) => {
-    console.log(`New player, tag: ${member.user.tag}`); 
     const welcomeNoTag = `Welcome to the server! We don't recognize your discord tag! If you're interested in joining the human or zombie channels, please go to the current game's page at www.uchicagohvz.org and enter your discord tag!`;
     client.playerdb.find_user(member.user.tag, row =>{
         if(row){
