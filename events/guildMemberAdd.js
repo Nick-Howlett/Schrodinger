7d870afe0215eeb8d5a51aacb586a2e87fbadbf0
unknown <agiel882@gmail.com>
2019-02-24 14:54:23 -0800

    Debugging guildMemberAdd event

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,5 @@
 module.exports = (client, member) => {
+    console.log(`New player, tag: ${member.user.tag}`); 
     const welcomeNoTag = `Welcome to the server! We don't recognize your discord tag! If you're interested in joining the human or zombie channels, please go to the current game's page at www.uchicagohvz.org and enter your discord tag!`;
     const welcomeTag = `Welcome to the server! You have been assigned a role of ${row.human ? "human" : "zombie"} based on your discord tag!`
     client.playerdb.find_user(member.user.tag, row =>{
