014f004cfd69bb72e5f30f9d7ff129d48122c08f
unknown <agiel882@gmail.com>
2019-02-25 19:50:23 -0800

    Remove attemped markdown

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     const keys = client.commands.filter(command => command.perms == 0).keyArray();
-    const commandString = `Server commands are as follows: \n * ${keys.join("\n -")}`;
+    const commandString = `Server commands are as follows: \n -${keys.join("\n -")}`;
     message.channel.send(commandString);
 }
 
